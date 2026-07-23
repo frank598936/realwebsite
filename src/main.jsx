9ec3710f693976dec23da1c4c3eab99a1881c627
frank598936 <fw598936@gmail.com>
2026-07-23 04:25:47 -0700

    Add Tawk.to live chat widget

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext.jsx";
 import { TransactionProvider } from "./context/TransactionContext.jsx";
+// Tawk.to Chat Widget
+
+const tawkScript = document.createElement("script");
+
+tawkScript.async = true;
+
+tawkScript.src = "https://embed.tawk.to/6a61f75385c9821d4774b1e7/1ju7asc0a";
+
+tawkScript.charset = "UTF-8";
+
+tawkScript.setAttribute("crossorigin", "*");
+
+document.body.appendChild(tawkScript);
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
